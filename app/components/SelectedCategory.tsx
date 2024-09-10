c0eab9e945f6e36196f60cdcbabc8732126578a2
peco <sorpwjdafh@naver.com>
2024-09-10 22:06:14 +0900

    feat(prisma): add prisma

---
+++ app/components/SelectedCategory.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Card, CardHeader } from "@/components/ui/card";
+import { categoryItems } from "../lib/categoryItems";
+import Image from "next/image";
+import { useState } from "react";
+
+export function SelectCategory() {
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+
+  return (
+    <div className="grid grid-cols-4 gap-8 mt-10 w-3/5 mx-auto">
+      {categoryItems.map((item) => (
+        <div
+          key={item.id}
+          className="cursor-pointer flex flex-col items-center"
+        >
+          <Card>
+            <CardHeader>
+              <Image
+                src={item.imageUrl}
+                alt={item.name}
+                height={32}
+                width={32}
+                className="w-8 h-8"
+              />
+              <h3 className="font-medium">{item.title}</h3>
+            </CardHeader>
+          </Card>
+        </div>
+      ))}
+    </div>
+  );
+}
--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -2,6 +2,7 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { MenuIcon } from "lucide-react";
@@ -13,6 +14,7 @@ import {
   LoginLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import Link from "next/link";
 
 export async function UserNav() {
   const { getUser } = getKindeServerSession();
@@ -36,6 +38,29 @@ export async function UserNav() {
       <DropdownMenuContent align="end" className="w-[200px]">
         {user ? (
           <>
+            <DropdownMenuItem>
+              <form className="w-full">
+                <button type="submit" className="w-full text-start">
+                  Airbnb your Home
+                </button>
+              </form>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="my-homes" className="w-full">
+                My Listings
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="favorites" className="w-full">
+                My Favorites
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="reservations" className="w-full">
+                My Reservations
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
             <DropdownMenuItem>
               <LogoutLink className="w-full">Logout</LogoutLink>
             </DropdownMenuItem>
